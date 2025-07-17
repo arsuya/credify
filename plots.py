c1091305f0b141a848461415bcfd8b333b98e861
arsuya <arsuya16@gmail.com>
2025-07-17 13:57:49 +0700

    update

--- plots.py
+++ plots.py
@@ -406,7 +406,9 @@ def EDA_7_1 (data):
 
 def EDA_7_2 (data):
     income_skew = skew(data['income'].dropna())
-    st.metric(label=f"Skewness of Income", value=f"{income_skew:.4f}")
+    col1, col2, col3 = st.columns([1, 2, 1])  
+    with col2:  
+        st.metric(label="Skewness of Income", value=f"{income_skew:.4f}")
 
 def EDA_8_1 (data):
     # Step 1: Map target variable
